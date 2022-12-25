e4dd325f33b81730c31c680ced3c58a6458eab08
Hassan_Ali <moha813@gmail.com>
2022-12-25 10:04:02 -0600

    initial commit

---
+++ routes/score/score.service.js
@@ -0,0 +1,26 @@
+const Score = require("./score.model");
+
+const getScores = (gameId) => Score.find({ gameId });
+
+const addScore = ({ gameId, userId, score }) => {
+  const newScore = new Score({
+    gameId,
+    userId,
+    score,
+  });
+  return newScore.save();
+};
+
+const updateScore = ({ gameId, userId, score }) => {
+  return Score.findOneAndUpdate({ gameId, userId }, { score }, { new: true });
+};
+
+const deleteScores = ({ gameId, userId }) =>
+  Score.findOneAndDelete({ gameId, userId });
+
+module.exports = {
+  getScores,
+  addScore,
+  updateScore,
+  deleteScores,
+};
